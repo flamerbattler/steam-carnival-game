fd9f6c3a00a9834200a4e7d194eb12c4679ed986
Faaris Sanjar Musfaq <musfaqf@gmail.com>
2024-01-25 00:23:59 +0600

    final changes prob

--- flipgame.js
+++ flipgame.js
@@ -1,18 +1,31 @@
 const maximumPointsGiven = 6;
 const mininumPointsGiven = 1;
-const flipPercentageReduction = 5;
-const goalPoints = 40;
-let flipPercentage = 100;
-let flips = 20;
+const flipPercentageChange = 5;
+const goalPoints = 30;
+let flipPercentageForPoint = 99;
+let flipPercentageForZero = 1;
+let turnsLeft = 20;
 let pointTotal = 0;
 
 const flipOdds = percentage => Math.random() * 100 < percentage;
 const pointsToGive = () => Math.floor(Math.random() * (maximumPointsGiven - mininumPointsGiven + 1)) + mininumPointsGiven;
 
 function onButtonClick() {
-    const isPoint = flipOdds(flipPercentage);
-    flipPercentage -= flipPercentageReduction;
-    flips -= 1
+    const isPoint = flipOdds(flipPercentageForPoint);
+    turnsLeft -= 1
+
+    if (flipPercentageForPoint - flipPercentageChange < 0) {
+        flipPercentageForPoint = 0
+    } else {
+        flipPercentageForPoint -= flipPercentageChange;
+    };
+
+    if (flipPercentageForZero + flipPercentageChange > 100) {
+        flipPercentageForZero += 100
+    } else {
+        flipPercentageForZero += flipPercentageChange
+
+    }
 
     if (isPoint) {
         pointTotal += pointsToGive();
@@ -22,5 +35,6 @@ function onButtonClick() {
         }
     } else {
         pointTotal = 0;
+        // do lose
     }
 };
